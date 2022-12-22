42e68ea8ee216958da44520a45085dfedec01bdb
MrLinna <78093433+MrLinna@users.noreply.github.com>
2022-12-22 15:17:58 +0200

    clean up

--- part2/data_for_countries/src/components/RenderCountryInfo.js
+++ part2/data_for_countries/src/components/RenderCountryInfo.js
@@ -2,19 +2,21 @@ import OneCountryInfo from "./OneCountryInfo"
 import Button from "./Button"
 const RenderCountryInfo =({countries, setCountriesToShow}) => {
     const len = countries.length
+
     if (len === 0)
-      return (<div>No countries to show</div>)
+      return (<div>No matches, specify another filter</div>)
+
     if (len > 10)
       return(<div>Too many matches, specify another filter</div>)
   
     if( 1 < len && len <= 10)
-      return(countries.map(eachCountry => 
-                      <div key={eachCountry.name.common}>
-                        {eachCountry.name.common}
-                        <Button text="show" handleClick={()=>{setCountriesToShow([eachCountry])}}/>
+      return(countries.map(country => 
+                      <div key={country.name.common}>
+                        {country.name.common}
+                        <Button text="show" handleClick={()=>{setCountriesToShow([country])}}/>
                       </div>))
     if( 1 === len )
       return(<OneCountryInfo country = {countries[0]}/>)
-  }
+}
 
 export default RenderCountryInfo
--- part2/data_for_countries/src/components/WeatherForm.js
+++ part2/data_for_countries/src/components/WeatherForm.js
@@ -4,24 +4,22 @@ const WeatherForm =({city}) => {
     const [windSpeed, setWindSpeed] = useState("")
     const [temperature, setTemperature] = useState("")
     const [icon, setIcon] = useState("")
-    
-    const api_key = process.env.REACT_APP_API_KEY
-        useEffect (() => {
+    useEffect (() => {
         axios
-            .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
+            .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
             .then(response => {
                 setTemperature(response.data.main.temp)
                 setWindSpeed(response.data.wind.speed)
                 setIcon(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
             })
-        },[])
+    },[city])
+    
     return(
         <>
             <h2>Weather in {city}</h2>
             <div>{`temperature ${temperature} \xB0C`}</div>
-            <img src={icon}/>
+            <img src={icon} alt = {"Weather icon"}/>
             <div>wind {windSpeed} m/s</div>
-
         </>
     )
 }
