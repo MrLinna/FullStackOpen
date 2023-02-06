4d52b4f890dbf88c73a6a5efd513a2fedf6353cf
MrLinna <linnanantti@outlook.com>
2023-02-06 17:14:25 +0200

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,17 @@
 const dummy = () => 1
 
+const totalLikes = (blogs) => {
+  const likes = ( blogs ) => {
+    return blogs.map(blog => blog.likes)
+
+  }
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return likes(blogs).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
