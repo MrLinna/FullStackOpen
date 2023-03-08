92be83d905690ce932fb42dd00bf9cd809c3a664
MrLinna <linnanantti@outlook.com>
2023-03-08 11:27:38 +0200

    exercise 7.1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BrowserRouter as Router,
+         Routes, Route, Link } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +8,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <a href='/' style={padding}>anecdotes</a>
+      <a href='create' style={padding}>create new</a>
+      <a href='about' style={padding}>about</a>
     </div>
   )
 }
@@ -123,14 +125,22 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu />
+      </div>
+
+      <Routes>
+        <Route path="/" element={ <AnecdoteList anecdotes={anecdotes} /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/create" element={ <CreateNew addNew={addNew} /> } />
+        
+
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   )
 }
 
