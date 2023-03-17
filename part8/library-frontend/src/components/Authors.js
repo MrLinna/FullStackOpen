e67c62b212854d88ce16a8a560657aa4336fcbaf
MrLinna <linnanantti@outlook.com>
2023-03-17 13:49:44 +0200

    exercise 8.11

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,5 +1,48 @@
-import { ALL_AUTHORS } from '../queries'
-import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries'
+import { useQuery, useMutation } from '@apollo/client'
+import { useState } from 'react'
+
+const SetBirth = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [changeBornYear] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+    if (name !== '' && born !== '') {
+      changeBornYear({ variables: { name, born } })
+      setName('')
+      setBorn('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name{' '}
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born{' '}
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(Number(target.value))}
+            type="number"
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
@@ -11,7 +54,7 @@ const Authors = (props) => {
   if (result.loading) {
     return <div>loading...</div>
   }
-  const authors = result.data.allAuthors
+  const authors = result.data.allAuthors || []
 
   return (
     <div>
@@ -32,6 +75,7 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <SetBirth />
     </div>
   )
 }
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -40,3 +40,13 @@ export const CREATE_BOOK = gql`
     }
   }
 `
+
+export const UPDATE_AUTHOR = gql`
+  mutation editAuthor($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      born
+      id
+    }
+  }
+`
