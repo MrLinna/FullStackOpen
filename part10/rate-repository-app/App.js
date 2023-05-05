c72125781777da4f34229f8da04bea27f57e4e76
MrLinna <linnanantti@outlook.com>
2023-05-05 13:52:26 +0300

    exercise 10.2

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>päivittyykö heti?</Text>
+      <Text>päivittyykö heti???</Text>
       <StatusBar style="auto" />
     </View>
   );
